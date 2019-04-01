494e198ae98030b9c5319e7ff673aa83d0ad107e
Hugo <hugo.salt@protonmail.com>
2019-04-01 18:44:37 +0200

    draft progress

--- Proj2/Hugo/framework.py
+++ Proj2/Hugo/framework.py
@@ -16,8 +16,8 @@ def xavier_init(tensor, nonlinearity):
         gain = 5.0 / 3
     elif nonlinearity == 'relu':
         gain = math.sqrt(2.0)
-    else :
-        raise ValueError("nonlinearity must be 'none', 'relu' or 'tanh'")  
+    else:
+        raise ValueError("nonlinearity must be 'none', 'relu' or 'tanh'")
     std = gain * math.sqrt(2.0 / (tensor.size(1)+tensor.size(0)))
     return tensor.normal_(0, std)
 
@@ -26,77 +26,141 @@ def xavier_init(tensor, nonlinearity):
 # - Modules -
 #
 
-# Convention : any tensor (whereas it is data (forward) of gradients (backward))
-# always has a dimension of N*D where N are the number of samples of the batch 
+# Convention : any tensor (whereas it's data (forward) of gradients (backward))
+# always has a dimension of N*D where N are the number of samples of the batch
 # and D the dimentions of the sample (at that stage of the network).
 # ( the course slides are using the Jacobian notation i.e D*N )
 
 
 class Module(object):
-    
+
     def forward(self, *input):
         raise NotImplementedError
-        
+
     def backward(self, *gradwrtoutput):
         raise NotImplementedError
-        
+
     def param(self):
         return []
 
 
 class Linear(Module):
-    
+
     def __init__(self, in_feature_nb, out_feature_nb, nonlinearity_init):
         super(Linear, self).__init__()
         self.in_feature_nb = in_feature_nb
         self.out_feature_nb = out_feature_nb
+        # Initialy undefined gradients
+        self.w_grad = None
+        self.bias_grad = None
         # Parameter (weights and bias) values
         self.w = torch.empty(out_feature_nb, in_feature_nb)
         self.bias = torch.empty(out_feature_nb)
         # Initialize parameters
         xavier_init(self.w, nonlinearity_init)
         self.bias.zero_()
-        
+
     def forward(self, input):
-        self.last_input = input
+        self.last_input = input.clone()
         return input @ self.w.t() + self.bias
-        
+
     def backward(self, gradwrtoutput):
-        self.bias_grad = gradwrtoutput # loss gradient wrt bias
-        self.w_grad =  gradwrtoutput.t() @ self.last_input # loss gradient wrt w
-        return (self.w.t() @ gradwrtoutput.t()) # loss gradient wrt input
-        
+        self.bias_grad = gradwrtoutput  # loss gradient wrt bias
+        self.w_grad = gradwrtoutput.t() @ self.last_input  # loss gradient wrt w
+        return (self.w.t() @ gradwrtoutput.t())  # loss gradient wrt input
+
     def param(self):
         return [(self.w, self.w_grad), (self.bias, self.bias_grad)]
-    
-    
+
+
 class ReLu(Module):
-    
+
     def __init__(self):
         super(ReLu, self).__init__()
-        
+
     def forward(self, input):
         # relu(x) is 0 if x < 0, x otherwise
-        return ( (a.sign()+1) / 2 ) * a
-    
+        return ((input.sign()+1) / 2) * input
+
     def backward(self, gradwrtoutput):
         # relu(x) derivative is 0 if x < 0, 1 otherwise
-        return (a.sign()+1) / 2
-    
+        return (gradwrtoutput.sign()+1) / 2
+
     def param(self):
         return []
-    
-    
+
+
+class Sequential(Module):
+
+    def __init__(self, modules_array):
+        super(Sequential, self).__init__()
+        self.modules_array = modules_array
+
+    def forward(self, input):
+        for module in self.modules_array:
+            input = module.forward(input)
+        return input
+
+    def backward(self, gradwrtoutput):
+        for module in reversed(self.modules_array):
+            gradwrtoutput = module.backward(gradwrtoutput)
+        return gradwrtoutput
+
+    def param(self):
+        parameter_array = []
+        for module in self.modules_array:
+            parameter_array.append(module.param())
+        return parameter_array
+
+
+class MSE(Module):
+
+    def __init__(self, targets):
+        super(MSE, self).__init__()
+        self.targets = targets
+        self.N = targets.shape[0]  # batch size
+
+    def forward(self, input):
+        self.last_input = input.clone()
+        return ((input - targets)**2).sum(dim=1).mean()
+
+    def backward(self, gradwrtoutput=1):
+        # loss (MSE) derivative wrt input
+        return gradwrtoutput*(-2/self.N)*(self.last_input-self.targets)
+
+    def param(self):
+        return []
+
+
 #
 # - Optimizer -
 #
 
 
 class Optimizer(object):
-    
+
     def zero_grad(self):
         raise NotImplementedError
-        
+
     def step():
         raise NotImplementedError
-    
+
+
+class SGD(Optimizer):
+
+    def __init__(self, parameters, lr):
+        super(SGD, self).__init__()
+        self.parameters = parameters
+        self.lr = lr  # learning rate
+
+    def zero_grad(self):
+        # Set gradients of all parameters to zero
+        for p in parameters:
+            for pair in p:
+                pair[1].zero_()
+
+    def step():
+        # Update parameters according to gradient and learning rate
+        for p in parameters:
+            for pair in p:
+                pair[0] -= pair[1]*lr
